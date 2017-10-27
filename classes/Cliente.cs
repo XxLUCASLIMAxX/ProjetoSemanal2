9ec25e821280c22edbb94db896c1c5eba9473295
lucas Lima <lucas-limagomes@hotmail.com>
2017-10-27 19:08:07 -0200

    venda

--- classes/Cliente.cs
+++ classes/Cliente.cs
@@ -1,45 +1,48 @@
 using System;
 
-namespace CadastroVendaPoo.classes{
+namespace CadastroVendaPoo.classes
+{
 
-/// <summary>
-/// A classe cliente possui informações sobre o cliente
-/// </summary>
-public class Cliente{
-    private string nome;
-    private string email;
-    private string cpf;
-
-    private DateTime datacadastro;
-
-/// <summary>
-/// constroi o objeto cliente
-/// </summary>
-   public Cliente(){
-    }
     /// <summary>
-    /// Constroi o objeto cliente e necessita de parametros
+    /// A classe cliente possui informações sobre o cliente
     /// </summary>
-    /// <param name="nome">String</param>
-    /// <param name="email">String</param>
-    /// <param name="cpf">String</param>
-    /// <param name="datacadastro">DateTime</param>
-    public Cliente(string nome,string email,string cpf,DateTime datacadastro)
+    public class Cliente
     {
-        this.nome=nome;
-        this.email=email;
-        this.cpf=cpf;
-        this.datacadastro=datacadastro;
-        
+        private string nome;
+        private string email;
+        private string cpf;
+
+        private DateTime datacadastro;
+
+        /// <summary>
+        /// constroi o objeto cliente
+        /// </summary>
+        public Cliente()
+        {
+        }
+        /// <summary>
+        /// Constroi o objeto cliente e necessita de parametros
+        /// </summary>
+        /// <param name="nome">String</param>
+        /// <param name="email">String</param>
+        /// <param name="cpf">String</param>
+        /// <param name="datacadastro">DateTime</param>
+        public Cliente(string nome, string email, string cpf, DateTime datacadastro)
+        {
+            this.nome = nome;
+            this.email = email;
+            this.cpf = cpf;
+            this.datacadastro = datacadastro;
+
+        }
+        public string Nome { get { return nome; } set { nome = value; } }
+        public string Email { get { return email; } set { email = value; } }
+        public string Cpf { get { return cpf; } set { cpf = value; } }
+        public DateTime Datacadastra { get { return datacadastro; } set { datacadastro = value; } }
+
+
+
+
     }
-    public string Nome { get{return nome;} set{nome=value;} }
-    public string Email { get{return email;} set{email=value;} }
-    public string Cpf { get{return cpf;} set{cpf=value;} }
-    public DateTime Datacadastra { get{return datacadastro;} set{datacadastro=value;} }
-
-
-
-
-}
 
 }
